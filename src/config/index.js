f145f71d3ce1d8b454f047c3f06c01ccb1e55d21
Navinda Chandrasiri <adilanga@gmail.com>
2023-01-03 20:30:28 +0530

    Env Config Update

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 module.exports = {
     APP_DB: process.env.APP_DB,
-    APP_DOMAIN: process.env.APP_DOMAIN,
     APP_PORT: process.env.APP_PORT,
     APP_SECRET: process.env.APP_SECRET,
     BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS,
