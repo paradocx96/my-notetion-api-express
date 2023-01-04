ab324dd14fd21b10c3d9dc11d524a106601f9379
Navinda Chandrasiri <adilanga@gmail.com>
2023-01-04 18:02:52 +0530

    Note Routes Impl

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./users');
+const noteRoutes = require('./notes');
 
 router.use('/users', userRoutes);
+router.use('/notes', noteRoutes);
 
 module.exports = router;
